0651799f29f26abc68dab70f28b4571a37845b41
Rishat Zaynullin <9217421+zrishat@users.noreply.github.com>
2018-12-26 15:57:36 +0500

    Create name.py

---
+++ name.py
@@ -0,0 +1,16 @@
+inputString = '3702130'
+MagicNumber = inputString[0]
+Payload = inputString[1:]
+Payload = Payload[::-1]
+alphabet = u"абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+OutputString = ''
+while len(Payload)>0:
+  CurrentSum = ''
+  if (len(Payload)==1):
+    CurrentSum = Payload[0]
+  else:
+    CurrentSum = Payload[0:2]
+  ElementIndex = int(CurrentSum) - int(MagicNumber)
+  ElementChar = alphabet[ElementIndex]
+  print ElementChar
+  Payload = Payload.replace(CurrentSum,'')
